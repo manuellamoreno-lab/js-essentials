b99e052fc3ef584f36b42181a81d1b1ea1665f7b
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-09-08 08:02:06 -0300

    Create exercicio12.js

---
+++ m2/exercicio12.js
@@ -0,0 +1,19 @@
+//let idade = 25;
+
+//if (true) {
+//    let idade = 30;
+//    console.log("Dentro do bloco:", idade); //30
+//}
+
+//console.log("Fora do bloco:", idade); //25
+
+let idade = 25;
+
+if (true) {
+    var idade = 30;
+    console.log("Dentro do bloco:", idade);
+}
+
+console.log("Fora do bloco:", idade);
+
+//da erro, nem roda o código
