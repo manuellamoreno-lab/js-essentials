03f00416359659183978441ad82870166eba4f87
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-09-08 07:58:06 -0300

    Create exercicio2.js

---
+++ m2/exercicio2.js
@@ -0,0 +1,10 @@
+//var 2nome; inválido (não pode começar com 2)
+var nome2;
+//var _idade; válido
+var _idade;
+//var cidade-natal; inválido (por conta do -)
+var cidade_natal;
+//var $preco; válido
+var $preco;
+//var let; inválido, let é um nome q tem uma função ja no js, entao nao pode ser usado como variavel
+var var_let;
