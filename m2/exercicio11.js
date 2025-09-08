96ec0da2e7cbe6d450d1b7671edca98663f0ab7e
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-09-08 08:01:39 -0300

    Create exercicio11.js

---
+++ m2/exercicio11.js
@@ -0,0 +1,21 @@
+//var nome = "Ana";
+
+//function mostrarNome() {
+    //var nome = "Carlos";
+    //console.log(nome); // vai sair carlos pq é o último que foi alterado
+//}
+
+//mostrarNome();
+//console.log(nome); // vai sair ana, pq carlos foi alterado só dentro do bloco
+
+var nome = "Ana";
+
+function mostrarNome() {
+    let nome = "Carlos";
+    console.log(nome); 
+}
+
+mostrarNome();
+console.log(nome); 
+
+//n mudou nada
