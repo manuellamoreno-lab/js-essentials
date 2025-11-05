94d9cedaf92eb0da46cea1c2b4b7d925debe576b
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-11-05 15:56:33 -0300

    Add comments explaining errors and exceptions in JS

--- m6/ex1.js
+++ m6/ex1.js
@@ -1,1 +1,3 @@
-
+// 1. Erro: é um problema que impede o código de continuar corretamente.
+// 2. Exceção: é um tipo específico de erro que pode ser “capturado” e tratado pelo programa.
+// 3. Diferença: um "erro" em linguagem natural é algo errado de modo geral; já em JavaScript, uma “exceção” é um objeto lançado (throw) durante a execução que pode ser tratado com try…catch.
