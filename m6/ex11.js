831b77eae62f94a24e734d18ca8d6cf191428d6a
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-11-12 09:26:25 -0300

    Update ex11.js

--- m6/ex11.js
+++ m6/ex11.js
@@ -1,1 +1,12 @@
+function testeDebug(x) {
+  const y = x * 2;
+  debugger; // pausa a execução aqui
+  return y;
+}
+testeDebug(5);
 
+/*
+Relatório:
+Quando a execução atinge 'debugger', o navegador pausa o código no painel 'Sources'.
+É possível inspecionar variáveis, avançar linha por linha e observar o fluxo do programa.
+*/
