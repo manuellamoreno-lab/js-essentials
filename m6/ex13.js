f6e7bbe2620eefaf6d5db5070bca1bacc5a4628f
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-11-12 09:27:00 -0300

    Refactor interno and externo functions with comments
    Add comments and improve function structure.

--- m6/ex13.js
+++ m6/ex13.js
@@ -1,1 +1,14 @@
+/*
+No momento em que 'interno' está sendo executado:
 
+▶ externo
+  ▶ interno
+*/
+
+function interno(m) {
+  return m * 3;
+}
+function externo(n) {
+  return interno(n) + 1;
+}
+externo(4);
