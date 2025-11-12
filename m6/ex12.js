c7019c282de34f6fb8bcf42ffdbd224d7371ec6e
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-11-12 09:26:38 -0300

    Implement interno and externo functions
    Add internal and external functions for multiplication and addition.

--- m6/ex12.js
+++ m6/ex12.js
@@ -1,1 +1,13 @@
+function interno(m) {
+  return m * 3;
+}
+function externo(n) {
+  return interno(n) + 1;
+}
+externo(4);
 
+/*
+- Step Into: entra na função 'interno' e mostra sua execução linha a linha.
+- Step Over: executa 'interno' sem entrar, indo direto para a próxima linha.
+- Step Out: sai da função atual e volta à que a chamou.
+*/
