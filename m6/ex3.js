0679b47c9d1e2bd8b6bf2c0701839573b71c62d9
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-11-12 09:24:15 -0300

    Add function to validate numeric input

--- m6/ex3.js
+++ m6/ex3.js
@@ -1,1 +1,10 @@
+function validarNumero(input) {
+  if (isNaN(Number(input))) {
+    console.log("Entrada inválida: número esperado.");
+  } else {
+    console.log("Número aceito:", Number(input));
+  }
+}
 
+validarNumero("abc");
+validarNumero("42");
