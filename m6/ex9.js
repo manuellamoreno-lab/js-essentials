6b69d50f4abbaa1addbb8758fdc628dc384ca3ea
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-11-12 09:25:57 -0300

    Update ex9.js

--- m6/ex9.js
+++ m6/ex9.js
@@ -1,1 +1,8 @@
+function soma(a, b) {
+  console.log("a:", a, "b:", b);
+  const resultado = a + b;
+  console.log("Resultado da soma:", resultado);
+  return resultado;
+}
 
+console.log(soma(2, undefined));
