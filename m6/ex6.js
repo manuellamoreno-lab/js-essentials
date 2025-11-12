45b587376a0538fcd174ff6e0f4ecd105b9c39b4
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-11-12 09:25:15 -0300

    Implement safeParse for safe JSON parsing
    Add safeParse function to handle JSON parsing safely

--- m6/ex6.js
+++ m6/ex6.js
@@ -1,1 +1,14 @@
+function safeParse(jsonString) {
+  try {
+    return JSON.parse(jsonString);
+  } catch (erro) {
+    if (erro instanceof SyntaxError) {
+      return null;
+    } else {
+      throw erro;
+    }
+  }
+}
 
+console.log(safeParse('{"nome": "Carol"}'));
+console.log(safeParse("texto inválido"));
