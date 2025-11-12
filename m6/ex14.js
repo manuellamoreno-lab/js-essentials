a227d6818ba6966faa3d544431793a62e0b8678d
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-11-12 09:27:15 -0300

    Update ex14.js

--- m6/ex14.js
+++ m6/ex14.js
@@ -1,1 +1,10 @@
+/*
+Para continuar a execução após um breakpoint:
+→ Clique no botão “Play” (Resume script execution) no painel do depurador.
 
+Para remover todos os breakpoints:
+→ Clique com o botão direito na lista de breakpoints e selecione “Remove all breakpoints”,
+ou use o menu do painel lateral “Breakpoints”.
+*/
+
+console.log("Dicas de depuração no comentário acima.");
