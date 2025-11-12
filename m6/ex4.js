2a639a958ddf65348a99580bf7b6c3eea58782cb
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-11-12 09:24:31 -0300

    Add error type explanations as comments
    Added comments explaining different types of errors in JavaScript.

--- m6/ex4.js
+++ m6/ex4.js
@@ -1,1 +1,5 @@
-
+/*
+ReferenceError: ocorre quando se tenta usar uma variável não declarada.
+TypeError: ocorre quando se tenta usar um tipo de dado de forma incorreta (ex: chamar número como função).
+SyntaxError: ocorre quando há erro na estrutura do código (ex: esquecer parêntese ou chaves).
+*/
