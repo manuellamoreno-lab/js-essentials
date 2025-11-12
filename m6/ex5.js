1aa98e0852932da91bb5ed78c543b68354993498
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-11-12 09:24:47 -0300

    Add safeParse function for JSON parsing

--- m6/ex5.js
+++ m6/ex5.js
@@ -1,1 +1,10 @@
+function safeParse(jsonString) {
+  try {
+    return JSON.parse(jsonString);
+  } catch {
+    return null;
+  }
+}
 
+console.log(safeParse('{"nome": "Carol"}')); 
+console.log(safeParse("texto inválido"));
