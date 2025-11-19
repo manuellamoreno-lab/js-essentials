7b84c1e9001c5fd8b5fd5807455fb9e0d053f414
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-11-19 10:21:27 -0300

    Add HTML structure and JavaScript array creation

---
+++ m4/ex11.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+    let comp = Number(prompt("Escolha o comprimento do array"))
+    let array = []
+    for(i=0;i<=comp;i++){
+        array[i] = i
+    }
+    alert(array)
+</script>
+</html>
